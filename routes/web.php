d29cec662f2d774ecc12144ca901d479832461f8
k3kan <aintbor@gmail.com>
2021-06-17 21:46:04 +0000

    add create article

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,8 @@ Route::get('about', [PageController::class, 'about']);
 
 Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
 
+Route::get('articles/create', [ArticleController::class, 'create'])->name('articles.create');
+
+Route::post('articles', [ArticleController::class, 'store'])->name('articles.store');
+
 Route::get('articles/{id}', [ArticleController::class, 'show'])->name('article.show');
